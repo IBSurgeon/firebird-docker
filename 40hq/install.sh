d90bdd0779c3f1ee6126e414d4b8f0f31151bb3c
Alexey Kovyazin <support@ibknowledgebase.com>
2024-06-03 22:12:23 +0200

    Docker files for HQbird

---
+++ 40hq/install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+FB_VER=4.0
+FTP_URL="https://cc.ib-aid.com/download/distr"
+TMP_DIR=$(mktemp -d)
+OLD_DIR=$(pwd -P)
+
+download_file(){
+    url=$1
+    tmp=$2
+    name=$3
+    fname=$(basename -- "$url")
+
+    echo "Downloading $name..."
+    curl $url --output $tmp/$fname --progress-bar
+
+    case $? in
+      0)  echo "OK";;	  
+      23) echo "Write error"
+          exit 0;;
+      67) echo "Wrong login / password"
+              exit 0;;
+      78) echo "File $fb_url/$fb_file $does not exist on server"
+          exit 0;;
+    esac
+}
+
+#apt update
+#apt install --no-install-recommends -y net-tools libtommath1 libicu70 wget unzip  gettext libncurses5 curl tar openjdk-8-jre jsvc tzdata locales sudo mc xz-utils file
+#ln -s libtommath.so.1 /usr/lib/x86_64-linux-gnu/libtommath.so.0
+#locale-gen "en_US.UTF-8"
+
+## Firebird & Hqbird download
+download_file $FTP_URL/$FB_VER/fb.tar.xz $TMP_DIR "FB installer"
+#download_file $FTP_URL/$FB_VER/conf.tar.xz $TMP_DIR "FB config files"
+download_file $FTP_URL/amvmon.tar.xz $TMP_DIR "AMV & MON installer"
+download_file $FTP_URL/distrib.tar.xz $TMP_DIR "DG installer"
+download_file $FTP_URL/hqbird.tar.xz $TMP_DIR "HQbird installer"
+
+echo Extracting FB installer ==================================================
+
+mkdir $TMP_DIR/fb $TMP_DIR/conf
+tar xvf $TMP_DIR/fb.tar.xz -C $TMP_DIR/fb --strip-components=1 > /dev/null
+#tar xvf $TMP_DIR/conf.tar.xz -C $TMP_DIR/conf  > /dev/null
+cd $TMP_DIR/fb
+
+echo Running FB installer =====================================================
+
+yes 'masterkey' | ./install.sh
+#./install.sh -silent
+cd $OLD_DIR
+#cp -rf $TMP_DIR/conf/*.conf /opt/firebird
+
+echo Installing HQbird ========================================================
+
+if [ ! -d /opt/hqbird ]; then 
+	echo "Creating directory /opt/hqbird"
+        mkdir /opt/hqbird
+    else
+	echo "Directory /opt/hqbird already exists"
+fi
+
+tar xvf $TMP_DIR/amvmon.tar.xz -C /opt/hqbird > /dev/null
+tar xvf $TMP_DIR/distrib.tar.xz -C /opt/hqbird > /dev/null
+tar xvf $TMP_DIR/hqbird.tar.xz -C /opt/hqbird > /dev/null
+
+if [ ! -d /opt/hqbird/outdataguard ]; then 
+	echo "Creating directory /opt/hqbird/outdataguard"
+	mkdir /opt/hqbird/outdataguard
+    else
+        echo "Directory /opt/hqbird/outdataguard already exists"
+fi
+echo "Running HQbird setup"
+sh /opt/hqbird/hqbird-setup
+rm -f /opt/firebird/plugins/libfbtrace2db.so 2 > /dev/null
+
+chown -R firebird:firebird /opt/hqbird /opt/firebird/firebird.conf /opt/firebird/databases.conf
+
+#echo Registering HQbird ========================================================
+#java -jar /opt/hqbird/dataguard.jar -register -regemail="saveincloudtrial@ib-aid.com" -regpaswd="25FCMZXC" -installid=/opt/hqbird/conf/installid.bin -unlock=/opt/hqbird/conf/unlock -license="E"
+
+sed -i 's/^#\s*RemoteAuxPort.*$/RemoteAuxPort = 3059/g' /opt/firebird/firebird.conf
+#sed -i 's/ftpsrv.homedir=/ftpsrv.homedir=\/opt\/database/g' /opt/hqbird/conf/ftpsrv.properties
+sed -i 's/ftpsrv.passivePorts=40000-40005/ftpsrv.passivePorts=40000-40000/g' /opt/hqbird/conf/ftpsrv.properties
+
+# cleanup
+if [ -d $TMP_DIR ]; then rm -rf $TMP_DIR; fi
---
+++ 50hq/start_svc.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/bin/java
+export DG_USER=firebird
+export TMP_DIR=/var/tmp
+
+OLD_DIR=$(pwd -P)
+
+# HQbird =========================
+
+cd /opt/hqbird
+/usr/bin/java -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Xms160m -Xmx512m -XX:+UseG1GC -jar dataguard.jar -config-directory=/opt/hqbird/conf -default-output-directory=/opt/hqbird/outdataguard/ &
+sleep 5
+
+# AMV ============================
+
+unset PID_FILE CLASSPATH DG_OPTS MON4_HOME
+export MON4_HOME=/opt/hqbird/amv
+export PID_FILE=/var/run/fbccamv.pid
+export DG_OPTS="-Djava.net.preferIPv4Stack=true -Xmx256m "
+export CLASSPATH=$MON4_HOME/amv.jar
+
+cd /opt/hqbird/amv
+/usr/bin/java -Xmx256m -cp amv.jar Main &
+sleep 5
+
+# Launcher =======================
+
+unset PID_FILE CLASSPATH DG_OPTS MON4_HOME
+export MON4_HOME=/opt/hqbird/mon
+export PID_FILE=/var/run/fbcclauncher.pid
+export DG_OPTS="-Djava.net.preferIPv4Stack=true -Xmx256m "
+export CLASSPATH=$MON4_HOME/bin/fbcclauncher.jar
+
+cd /opt/hqbird/mon/bin
+/usr/bin/java -Xmx256m  -jar fbcclauncher.jar &
+
+# TraceHorse =====================
+
+unset PID_FILE
+export MON4_HOME=/opt/hqbird/mon
+export PID_FILE=/var/run/fbcctracehorse.pid
+export DG_OPTS="-Djava.net.preferIPv4Stack=true -Xmx256m "
+export CLASSPATH=$MON4_HOME/bin/tracehorse.jar
+
+cd /opt/hqbird/mon/bin
+/usr/bin/java -Xmx256m -jar tracehorse.jar &
+
+# Firebird =======================
+# Start Firebird but do not daemonize
+
+#/opt/firebird/bin/fbguard -pidfile /var/run/firebird/firebird.pid -daemon -forever
+/opt/firebird/bin/fbguard -pidfile /var/run/firebird/firebird.pid -forever
+
+cd $OLD_DIR
